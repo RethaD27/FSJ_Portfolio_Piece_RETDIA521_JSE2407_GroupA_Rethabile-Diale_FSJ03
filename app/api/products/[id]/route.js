a5d0413ddce54ebac00b42f509b582be72779c3b
Rethabile <rethadiale27@gmail.com>
2024-10-05 12:24:31 +0200

    Updated import path for 'db' from '../../..firebase' to '@/firebase' in products/[id]/route.js

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,5 +1,5 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../..firebase';
+import { db } from '@/firebase';
 
 export default async function handler(req, res) {
   const { method, query: { id } } = req;
