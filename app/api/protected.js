4a76525a80ee159d37356363e5a728e2c6e77ab9
Rethabile <rethadiale27@gmail.com>
2024-10-08 10:34:54 +0200

    Added protected route handler with token verification middleware

---
+++ app/api/protected.js
@@ -0,0 +1,7 @@
+import { verifyIdToken } from '../../middleware/verifyToken';
+
+export default async function handler(req, res) {
+  await verifyIdToken(req, res, async () => {
+    res.status(200).json({ message: 'This is a protected route' });
+  });
+}
