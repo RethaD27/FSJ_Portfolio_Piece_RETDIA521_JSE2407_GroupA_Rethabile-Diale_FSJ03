317597c5ffbdc3dccb0610d3ae64f2eecfbf2e04
Rethabile <rethadiale27@gmail.com>
2024-10-08 10:31:50 +0200

    Added authentication functions for sign-up, sign-in, and sign-out with Firebase

---
+++ app/authFunctions.js
@@ -0,0 +1,32 @@
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const authStateListener = (callback) => {
+  return onAuthStateChanged(auth, callback);
+};
